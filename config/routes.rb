e3b2febdd14db968659bc8ecfd2b05baeb71584c
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-03-18 08:54:17 +0000

    AddDatabase-3.4.10.11

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,52 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :admin do
+    get 'orders/show'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/edit'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/show'
+    get 'items/edit'
+  end
+  namespace :admin do
+    get 'homes/top'
+  end
+  namespace :public do
+    get 'destinations/index'
+    get 'destinations/edit'
+  end
+  namespace :public do
+    get 'orders/new'
+    get 'orders/index'
+    get 'orders/show'
+    get 'orders/confirm'
+    get 'orders/thanks'
+  end
+  namespace :public do
+    get 'cart_items/index'
+  end
+  namespace :public do
+    get 'items/homes'
+  end
+  namespace :public do
+    get 'items/public/homes'
+  end
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
