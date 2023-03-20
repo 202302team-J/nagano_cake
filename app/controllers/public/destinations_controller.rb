8ebf482717d8acbd5368c1d6203c1a4f702f6bfc
hiroki <you@example.com>
2023-03-20 07:41:15 +0000

    destinations一部追加

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -1,7 +1,21 @@
 class Public::DestinationsController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
+    @destinations = current_customer.destinations
+    @destination = Destination.new
   end
 
   def edit
   end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
