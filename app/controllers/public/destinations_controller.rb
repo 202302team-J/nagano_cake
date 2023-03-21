3d900317ea3613d5e1b0aee60f1a97baaacbeb06
hiroki <you@example.com>
2023-03-21 06:30:15 +0000

    修正

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -3,7 +3,7 @@ class Public::DestinationsController < ApplicationController
 
   def index
       @destination = Destination.new
-      @destinations = destinations.all
+      @destinations = Destination.all
   end
 
   def edit
@@ -12,9 +12,8 @@ class Public::DestinationsController < ApplicationController
 
   def create
       destination = Destination.new(destination_params)
-      destination.customer_id = current_customer_id
+      destination.customer_id = current_customer.id
    if destination.save
-      flash[:notice] = '配送先を登録しました'
       redirect_to public_destinations_path
    else
       flash.now[:alert] = '必要な情報を入力してください'
@@ -25,12 +24,11 @@ class Public::DestinationsController < ApplicationController
   def update
       @destination = Destination.find(params[:id])
    if @destination.update(destination_params)
-      flash[:notice] = '変更内容を保存しました'
       redirect_to public_destinations_path
    else
       @destination = Destination.find(params[:id])
       flash.now[:alert] = '必要な情報を入力してください'
-      render :edit_public_destination
+      render :edit
    end
   end
 
@@ -43,7 +41,7 @@ class Public::DestinationsController < ApplicationController
   private
 
   def destination_params
-    params.require(:destination).permit(:postcord, :address, :name)
+    params.require(:destination).permit(:postcord, :address, :name, :customer_id)
   end
 
 end
