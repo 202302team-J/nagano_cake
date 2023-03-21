5d8ad51181c69f7babc8efd1bb3c62f7c4b02b59
kitajimakokoro <122000079+kitajimakokoro@users.noreply.github.com>
2023-03-21 03:17:25 +0000

    Add:商品の詳細画面作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
   has_many   :order_details
   has_many   :cart_item
   has_one_attached :image
+  
+  
+  #税込み価格を表示するための記述です。
+  #add_tax_(消費税を計算したいカラム名)
+  def add_tax_price
+  (self.price * 1.10).round
+  end
+  
 end
