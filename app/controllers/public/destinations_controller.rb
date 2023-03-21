f5bbc8ac1e8a4e7d2cd3b98613d065ce816f8c27
hiroki <you@example.com>
2023-03-21 01:50:20 +0000

    配送先一覧/編集画面作成

--- app/controllers/public/destinations_controller.rb
+++ app/controllers/public/destinations_controller.rb
@@ -2,20 +2,48 @@ class Public::DestinationsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @destinations = current_customer.destinations
-    @destination = Destination.new
+      @destination = Destination.new
+      @destinations = estinations.all
   end
 
   def edit
+      @destination = Destination.find(params[:id])
   end
 
   def create
+      destination = Destination.new(destination_params)
+      destination.customer_id = current_customer_id
+   if destination.save
+      flash[:notice] = '配送先を登録しました'
+      redirect_to  public_destination_index_path
+   else
+      flash.now[:alert] = '必要な情報を入力してください'
+      redirect_to  public_destination_index_path
+   end
   end
 
   def update
+      @destination = Destination.find(params[:id])
+   if @destination.update(destination_params)
+      flash[:notice] = '変更内容を保存しました'
+      redirect_to  public_destination_index_path
+   else
+      @destination = Destination.find(params[:id])
+      flash.now[:alert] = '必要な情報を入力してください'
+      render :public_destination_edit_path
+   end
   end
 
   def destroy
+      destination = Destination.find(params[:id])
+      destination.delete
+      redirect_to  public_destination_index_path
+  end
+
+  private
+
+  def destination_params
+    params.require(:destination).permit(:postcord, :address, :name)
   end
 
 end
