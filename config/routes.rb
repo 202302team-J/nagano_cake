da478e182f7e3d2c9603bca4b90da3a8291e1c41
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-03-21 08:20:58 +0000

    Add-customer-edit-deleted

--- config/routes.rb
+++ config/routes.rb
@@ -17,12 +17,16 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get 'homes/top'
+
   end
 
  namespace :public do
    get 'customers/show'
-   get 'customers/infomation/edit'
-   get 'customers/unsubscribe'
+   get 'customers/my_page' => 'customers#show', as: 'my_page'
+   get 'customers/information/edit' => 'customers#edit'
+   patch 'customers/information/edit' => 'customers#update', as: 'edit_my_page'
+   get 'customers/unsubscribe' =>'customers#unsubscribe', as: 'unsubscribe_my_page'
+   patch 'customers/is_deleted' => 'customers#is_deleted'
  end
 
   namespace :public do
@@ -51,11 +55,12 @@ Rails.application.routes.draw do
   namespace :public do
     get 'items/public/homes'
   end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/infomation/edit'
-    get 'customers/unsubscribe'
-  end
+  # namespace :public do
+  #   get '/customers/my_page' => 'customers#show', as: 'my_page'
+  #   get '/customers/information/edit' => 'customers#edit', as: 'edit_my_page'
+  #   get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe_my_page'
+  # end
+
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
