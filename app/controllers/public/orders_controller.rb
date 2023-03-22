cbe48febac2c5970f197be94d46a8da97f896db2
hiroki <you@example.com>
2023-03-22 04:13:04 +0000

    注文情報入力画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,9 +3,8 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order = Order.new
-    @orders = current_customer.orders.all
     @customer = Customer.find(current_customer.id)
-    @addresses = @customer.addresses
+    @destinations = current_customer.destinations.all
   end
 
   def index
@@ -17,6 +16,18 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
+      @order = Order.new(order_params)
+
+   if params[:order][:address_option] == "0" # [:address_option]=="0"を呼び出す
+      @order.shi_postcord = current_customer.postcord
+      @order.ship_address = current_customer.address
+      @order.ship_name = current_customer.last_name + current_customer.first_name
+
+   elsif params[:order][:address_option] == "1" # [:address_option]=="1"を呼び出す
+      ship = Address.find(params[:order][:customer_id])
+      @order.ship_postcord = ship.postcord
+      @order.ship_address = ship.address
+      @order.ship_name = ship.name
   end
 
   def thanks
@@ -24,7 +35,7 @@ class Public::OrdersController < ApplicationController
 
    private
 
-  def order_params
-    params.require(:order).permit(:payment_method, :name, :postcord, :address)
-  end
+    def order_params
+        params.require(:order).permit(:postage, :payment_method, :ship_name, :ship_address, :ship_postcord, :customer_id,:charge,:status)
+    end
 end
