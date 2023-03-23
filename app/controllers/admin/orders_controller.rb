14f68bc96a53555fe1b87d4f127fe6e7637f5496
HAYAMIfan <r.k.tempest1@gmail.com>
2023-03-23 14:28:48 +0000

    残すは連動

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,10 @@
 class Admin::OrdersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def show
     @order = Order.find(params[:id])
     @total = 0
-    @order.order_details.each do |detail| 
+    @order.order_details.each do |detail|
       @total = @total+detail.subtotal
     end
   end
@@ -12,7 +12,7 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order.update(order_params)
-    redirect_to admin_customers_path(@order.customer)
+    redirect_to admin_order_path(@order)
   end
 
   private
