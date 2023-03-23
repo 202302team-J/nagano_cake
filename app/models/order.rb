887f6b8713a60f0cf15f1e1fa7683d417c01ae29
hiroki <you@example.com>
2023-03-23 07:06:44 +0000

    注文履歴詳細作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,8 @@ class Order < ApplicationRecord
   has_many   :order_details
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting_for_payment: 0, payment_confirmation: 1, in_production: 2, preparing_for_shipment: 3, shipped: 4 }
+
+  def tax_price
+    (item.price * 1.1)
+  end
 end
