be5d5e90ab850d0aaf7c01a1756164a6fc972272
hiroki <you@example.com>
2023-03-24 05:49:31 +0000

    画像関係修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,10 @@ class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting_for_payment: 0, payment_confirmation: 1, in_production: 2, preparing_for_shipment: 3, shipped: 4 }
 
+  def get_image(width,height)
+     image.variant(resize_to_limit: [width,height]).processed
+  end
+
   def add_tax_price
     (self.price*1.10).round
   end
