a250ca9e79a0bf2f815693d1dcd90155a6809be4
hiroki <you@example.com>
2023-03-24 09:59:36 +0000

    レイアウト調整

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -58,7 +58,7 @@ class Public::OrdersController < ApplicationController
 
       if @order.save
         current_customer.cart_items.each do |cart_item| #カートの商品を1つずつ取り出しループ
-        @ordered_item = Order.new (order_id :@order.id)#初期化宣言
+       @ordered_item = Order.new (order_id :@order.id)#初期化宣言
         ordered_item.item_id = cart_item.item_id #商品idを注文商品idに代入
         ordered_item.item_count = cart_item.item_count #商品の個数を注文商品の個数に代入
         ordered_item.add_tax_price = (cart_item.add_tax_price).round #消費税込みに計算して代入
