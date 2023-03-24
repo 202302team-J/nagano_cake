8288ccd3adbccdb2d86c90ee40c0f1aafc2da07b
HAYAMIfan <r.k.tempest1@gmail.com>
2023-03-24 10:46:39 +0000

    残すは編集

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,5 +8,24 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+    if @item.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render :edit
+    end
+  end
+
+ private
+
+  def customer_params
+    params.require(:customer).permit(:family_name,
+                                     :personal_name,
+                                     :family_name_ruby,
+                                     :personal_name_ruby,
+                                     :postcode, :address,
+                                     :telephone_number,
+                                     :email,
+                                     :is_deleted)
   end
 end
