a3f65ec4b8c16028ff524c2c494afad1bdfd35bb
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-03-24 07:06:35 +0000

    customer-top-Add

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,10 @@
 class Public::HomesController < ApplicationController
   
   def top
+    @items = Item.order('id DESC').limit(4)
+    @genres = Genre.all
   end
+  #ABCで古い順、DESCで新しい順、limitはとってくる限界を決めることができる
   
   def about
   end
