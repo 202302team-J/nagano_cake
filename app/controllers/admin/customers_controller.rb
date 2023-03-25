9f3aeb44e485c5b6b92f3ba1243ce8c56bc18d3e
HAYAMIfan <r.k.tempest1@gmail.com>
2023-03-25 08:22:56 +0000

    レイアウト

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -9,6 +9,10 @@ class Admin::CustomersController < ApplicationController
 
   def edit
     @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
       redirect_to admin_customer_path(@customer), notice: "正常に更新されました。"
     else
