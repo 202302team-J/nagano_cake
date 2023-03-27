2ada959a61e0c4557e43e30776dd023bd556a98e
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-03-27 05:18:54 +0000

    hama-cart-link

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -10,17 +10,20 @@ class Public::CartItemsController < ApplicationController
     # where・・1つ以上持ってこれる
     @cart_items = CartItem.where(customer_id: current_customer)
     @cart_item = CartItem.new(cart_item_params)
-
   # find_by・・findとは違って、id以外のデータを持ってこれる　「もしカート内に同じ商品があるなら足す、違うなら作る」という記述
-    if CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: params[:cart_item][:customer_id]).present?
-     cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: params[:cart_item][:customer_id])
-     cart_item.count += params[:cart_item][:count].to_i
-     cart_item.update_attribute(:count,cart_item.count)
-    else
-     cart_item = CartItem.new(cart_item_params)
-     cart_item.save
-    end
-     redirect_to  items_path
+     if CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: params[:cart_item][:customer_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id], customer_id: params[:cart_item][:customer_id])
+      cart_item.count += params[:cart_item][:count].to_i
+      cart_item.update_attribute(:count,cart_item.count)
+     else
+      cart_item = CartItem.new(cart_item_params)
+      cart_item.save
+     end
+      if cart_item.save
+      redirect_to  cart_items_path
+      else
+      redirect_to  items_path
+      end
   end
 
 
